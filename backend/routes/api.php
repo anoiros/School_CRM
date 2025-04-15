81b2102a5eea7f7bfb733fa607f963436df0f2d2
anoiros <157161713+anoiros@users.noreply.github.com>
2025-04-15 17:03:56 +0100

    Update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,9 +7,6 @@ use App\Http\Controllers\AuthController;
 // Route de connexion pour l'authentification (Login)
 Route::post('login', [AuthController::class, 'login']);
 
-// Route de déconnexion (Logout), nécessite un token valide
-Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
-
 Route::get('/', function () {
     return response()->json(['message' => 'API Laravel OK ']);
 });
